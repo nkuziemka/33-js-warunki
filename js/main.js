637ab5380f594e32e5f34d9258f7584267149e62
nat <natkuziemka@gmail.com>
2022-11-16 20:29:31 +0100

    Rozpoczecie projektu

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let number=12;
+
+if (number>10) {
+    console.log('Podana liczba jest większa niż 10', number>10);
+} else if (number<10) {
+    console.log('Podana liczba nie jest większa niż 10', number<10);
+}
